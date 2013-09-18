49a5a49e6c59f892e8cdf5cb24d096fffe0ca3a8
twitish <soppyhankins@gmail.com>
2013-09-18 17:52:04 +0000

    Initialize SimpleKernel for Android 4.2.2

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Sep 15 11:38:45 UTC 2013"
+#define LINUX_COMPILE_BY "twitish"
+#define LINUX_COMPILE_HOST "EdgeFire"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,2 @@
+#define KERNEL_VER_NOSCM "3.0.8"
+#define UTS_RELEASE "3.0.8-g85cbe81-dirty"
